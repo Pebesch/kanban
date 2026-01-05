04527bb271ee673ff6ec747c02f95004a466377b
Peter Schmucki <pebs@gmx.ch>
2026-01-05 13:38:38 +0100

    Added board and restirictions

--- core/src/main/java/ch/schmucki/core/KanbanBoard.java
+++ core/src/main/java/ch/schmucki/core/KanbanBoard.java
@@ -2,6 +2,5 @@ package ch.schmucki.core;
 
 import java.util.List;
 
-public class User {
-    private String name;
+public class KanbanBoard {
 }
--- core/src/main/java/ch/schmucki/core/Project.java
+++ core/src/main/java/ch/schmucki/core/Project.java
@@ -4,4 +4,8 @@ import java.util.List;
 
 public class Project {
     private String name;
+    private KanbanBoard kanbanBoard;
+    private int maxNumberOfItemsSelected = 5;
+    private int maxNumberOfItemsProgress = 3;
+    private int maxNumberOfItemsReview = 3;
 }
