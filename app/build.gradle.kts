79630dedf96c9d20a0c08e5c2464abe1fe33e363
Peter Schmucki <pebs@gmx.ch>
2026-01-26 20:57:50 +0100

    Fixed h2 setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,14 @@
 plugins {
-    java
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "ch.schmucki"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-h2console:4.0.2")
 }
