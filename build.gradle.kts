e2ef5b8dbf0bddbc74d19c68ae0a00912fd90cd8
Peter Schmucki <pebs@gmx.ch>
2026-02-12 17:30:21 +0100

    Added spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ import net.ltgt.gradle.errorprone.errorprone
 plugins {
     id("java")
     id("idea")
-    alias(libs.plugins.errorprone)
+    alias(libs.plugins.errorprone) apply false
+    alias(libs.plugins.spotless) apply false
 }
 
 group = "ch.schmucki"
@@ -27,6 +28,17 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "java-library")
     apply(plugin = "net.ltgt.errorprone")
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            googleJavaFormat("1.34.1")
+            importOrder()
+            removeUnusedImports()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
 
     dependencies {
         testImplementation(rootProject.libs.junit.jupiter)
